2511fe1905ef14ab55b3bb652e22488b9dfe63d9
Angela Salazar <angelasalazargarcia04@gmail.com>
2022-12-07 11:16:16 +0100

    delete task implemented

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use("/api", projectRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const taskRoutes = require("./routes/task.routes");
+app.use("/api", taskRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -23,4 +23,22 @@ router.post("/tasks", (req, res, next) => {
       .catch((err) => res.json(err));
   });
 
+  //////////// N E W   T A S K ///////////
+
+  router.post("/tasks/:id/delete", (req, res, next)=>{
+    const id = req.params.id
+    console.log(id)
+    Task.findByIdAndRemove(id)
+    .then((deletedTask) =>{
+        console.log("MADE IT", deletedTask)
+    })
+    .catch((err) =>{
+        console.log(err)
+    }) 
+
+})
+
+/* console.log("this is ptoject: ", projectId)
+console.log("this is book creator: ", response.creator._id.toString() ) */
+
   module.exports = router;
