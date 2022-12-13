eee5d43340b8f4459436cd3952f679723276997c
Carolin Rieche <Ccarolin.rieche@yahoo.de>
2022-12-13 17:39:06 +0100

    put cruds and done logic

--- models/Task.model.js
+++ models/Task.model.js
@@ -11,7 +11,7 @@ const taskSchema = new Schema(
     dueDate: Date,
     project: { type: Schema.Types.ObjectId, ref: "Project" },
     important: Boolean,
-    checkTask: Boolean,
+    done: Boolean,
     createdBy: { type: Schema.Types.ObjectId, ref: "User" },
   },
   {
