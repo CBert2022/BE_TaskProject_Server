c3a29edf4f615c29301500de103c2f5df0618173
Carolin Rieche <Ccarolin.rieche@yahoo.de>
2022-12-16 10:05:31 +0100

    fix localhost

--- config/index.js
+++ config/index.js
@@ -22,11 +22,12 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: [FRONTEND_URL],
+  //     credentials: true,
+  //   })
+  // );
 
   // In development environment the app logs
   app.use(logger("dev"));
